6cb8b9d43e022b4eefa107a65bbcbc5adeb21093
jiangwenyang <wenyang.jay@gmail.com>
2025-06-23 17:09:35 +0800

    feat: 添加首屏加载动画

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,16 @@
 
 <script setup>
 import { NConfigProvider, zhCN, dateZhCN } from 'naive-ui'
-
 /**
-   * js 文件下使用这个做类型提示
-   * @type import('naive-ui').GlobalThemeOverrides
-   */
-  const themeOverrides = {
-    common: {
-      primaryColor: '#0C53FD',
-      primaryColorHover: '#0038FF',
-      primaryColorPressed: '#0032E6',
-      primaryColorSuppl: '#E0E8FF',
-    },
-
-  }
+ * js 文件下使用这个做类型提示
+ * @type import('naive-ui').GlobalThemeOverrides
+ */
+const themeOverrides = {
+  common: {
+    primaryColor: '#0C53FD',
+    primaryColorHover: '#0038FF',
+    primaryColorPressed: '#0032E6',
+    primaryColorSuppl: '#E0E8FF',
+  },
+}
 </script>
